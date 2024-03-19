6e09eb0466a06846735d386f89d5ac2f86709e80
이영선 <leedrkr323@konkuk.ac.kr>
2024-03-19 09:50:49 +0900

    Initial commit

---
+++ src/test/java/kuchat/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kuchat.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
